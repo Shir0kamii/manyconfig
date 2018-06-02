2074785e4f5bb94f451fab6b121c44c415f1f6f3
Alexandre Bonnetain <shir0kamii@gmail.com>
2018-06-02 16:55:34 +0000

    Integrate bumpversion with documentation version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ author = 'Shir0kamii'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.0'
+version = '0.1.1'
 
 
 # -- Options for HTML output ----------------------------------------------
